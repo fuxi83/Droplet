168ba3bfffbf35ebc594d1b61364794e1ef9e8f2
stefan <stefan@10.0.60.148>
2018-07-03 10:29:28 +0200

    fixed wrong equals and hashcode for modelobjects

--- Droplet/src/main/java/com/stefanbrenner/droplet/model/internal/AbstractModelObject.java
+++ Droplet/src/main/java/com/stefanbrenner/droplet/model/internal/AbstractModelObject.java
@@ -23,49 +23,14 @@ import javax.xml.bind.annotation.XmlAccessorType;
 
 import com.jgoodies.binding.beans.Model;
 
-import lombok.Data;
-import lombok.EqualsAndHashCode;
-
 /**
  * Abstract base class for all model objects used in droplet.
- * 
+ *
  * @author Stefan Brenner
  */
-@Data
-@EqualsAndHashCode(callSuper = false)
 @XmlAccessorType(javax.xml.bind.annotation.XmlAccessType.FIELD)
 public abstract class AbstractModelObject extends Model {
 	
 	private static final long serialVersionUID = 1L;
-	
-	// private final PropertyChangeSupport propertyChangeSupport = new
-	// PropertyChangeSupport(
-	// this);
-	//
-	// public void addPropertyChangeListener(PropertyChangeListener listener) {
-	// propertyChangeSupport.addPropertyChangeListener(listener);
-	// }
-	//
-	// public void addPropertyChangeListener(String propertyName,
-	// PropertyChangeListener listener) {
-	// propertyChangeSupport.addPropertyChangeListener(propertyName, listener);
-	// }
-	//
-	// public void removePropertyChangeListener(PropertyChangeListener listener)
-	// {
-	// propertyChangeSupport.removePropertyChangeListener(listener);
-	// }
-	//
-	// public void removePropertyChangeListener(String propertyName,
-	// PropertyChangeListener listener) {
-	// propertyChangeSupport.removePropertyChangeListener(propertyName,
-	// listener);
-	// }
-	//
-	// protected void firePropertyChange(String propertyName, Object oldValue,
-	// Object newValue) {
-	// propertyChangeSupport.firePropertyChange(propertyName, oldValue,
-	// newValue);
-	// }
 	
 }
